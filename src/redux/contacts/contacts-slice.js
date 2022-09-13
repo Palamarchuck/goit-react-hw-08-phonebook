1c27484f369e7700bf5867829d988c4777ec4b00
Palamarchuck <palico55@gmail.com>
2022-09-13 20:46:34 +0300

    final

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -1,60 +1,103 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { combineReducers, createSlice } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, removeContact } from "./contacts-operations";
 
 // fetchContacts.pending - запит пішов
 // fetchContacts.fulfilled - відповідь успішна
 // fetchContacts.rejected - відповідь з помилкою
 
-
-
-const initialState = {
-    items: [],
-    loading: false,
-    error: null,
-};
-
-const contactsSlice = createSlice({
-    name: "contacts",
-    initialState,
+const itemsSlice = createSlice({
+    name: "items",
+    initialState: [],
     extraReducers: {
-        [fetchContacts.pending]: (store) => {
-            store.loading = true;
-            store.error = null;
-        },
-        [fetchContacts.fulfilled]: (store, { payload }) => {
-            store.loading = false;
-            store.items = payload;
-        },
-        [fetchContacts.rejected]: (store, { payload }) => {
-            store.loading = false;
-            store.error = payload;
-        },
-        [addContact.pending]:(store) => {
-            store.loading = true;
-            store.error = null;
-        },
-        [addContact.fulfilled]: (store, { payload }) => {
-            store.loading = false;
-            store.items.push(payload);
-        },
-        [addContact.rejected]: (store, { payload }) => {
-            store.loading = false;
-            store.error = payload;
-        },
-        [removeContact.pending]: (store) => {
-            store.loading = true;
-            store.error = null;
-        },
-        [removeContact.fulfilled]: (store, { payload }) => {
-            store.loading = false;
-            store.items = store.items.filter(item => item.id !== payload);
-        },
-        [removeContact.rejected]: (store, { payload }) => {
-            store.loading = false;
-            store.error = payload;
-        }
-        
+        [fetchContacts.fulfilled]: (_, { payload }) => payload,
+        [addContact.fulfilled]: (store, { payload }) => [...store, payload],
+        [removeContact.fulfilled]: (store, {payload}) => store.filter(item => item.id !== payload),
     }
 });
 
-export default contactsSlice.reducer;
+const loadingSlice = createSlice({
+    name: "loading",
+    initialState: false,
+    extraReducers: {
+        [fetchContacts.pending]: () => true,
+        [fetchContacts.fulfilled]: () => false,
+        [fetchContacts.rejected]: () => false,
+        [addContact.pending]: () => true,
+        [addContact.fulfilled]: () => false,
+        [addContact.rejected]: () => false,
+        [removeContact.pending]: () => true,
+        [removeContact.fulfilled]: () => false,
+        [removeContact.rejected]: () => false,
+    }
+});
+
+const errorSlice = createSlice({
+    name: "error",
+    initialState: null,
+    extraReducers: {
+        [fetchContacts.pending]: () => null,
+        [fetchContacts.rejected]: (_, { payload }) => payload,
+        [addContact.pending]: () => null,
+        [addContact.rejected]: (_, { payload }) => payload,
+        [removeContact.pending]: () => null,
+        [removeContact.rejected]: (_, {payload}) => payload,
+    }
+});
+
+const contactsReducer = combineReducers({
+    items: itemsSlice.reducer,
+    loading: loadingSlice.reducer,
+    error: errorSlice.reducer,
+})
+
+// const initialState = {
+//     items: [],
+//     loading: false,
+//     error: null,
+// };
+
+// const contactsSlice = createSlice({
+//     name: "contacts",
+//     initialState,
+//     extraReducers: {
+//         [fetchContacts.pending]: (store) => {
+//             store.loading = true;
+//             store.error = null;
+//         },
+//         [fetchContacts.fulfilled]: (store, { payload }) => {
+//             store.loading = false;
+//             store.items = payload;
+//         },
+//         [fetchContacts.rejected]: (store, { payload }) => {
+//             store.loading = false;
+//             store.error = payload;
+//         },
+//         [addContact.pending]:(store) => {
+//             store.loading = true;
+//             store.error = null;
+//         },
+//         [addContact.fulfilled]: (store, { payload }) => {
+//             store.loading = false;
+//             store.items.push(payload);
+//         },
+//         [addContact.rejected]: (store, { payload }) => {
+//             store.loading = false;
+//             store.error = payload;
+//         },
+//         [removeContact.pending]: (store) => {
+//             store.loading = true;
+//             store.error = null;
+//         },
+//         [removeContact.fulfilled]: (store, { payload }) => {
+//             store.loading = false;
+//             store.items = store.items.filter(item => item.id !== payload);
+//         },
+//         [removeContact.rejected]: (store, { payload }) => {
+//             store.loading = false;
+//             store.error = payload;
+//         }
+        
+//     }
+// });
+
+export default contactsReducer;
